60010b42462d6d07a72aaa8f6e133c44b98d5ee4
Mahmood Mahmood <mahmood.a.yahya@gmail.com>
2022-07-08 11:16:16 +0400

    Added three functional hint buttons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,20 +2,24 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <br>
-    <h2> Question #1: Xibu cbcb't kpc jt </h2>
+<h2> Answer the following question: Rwtrz pqdkt iwt ugxsvt </h2>
 
     <form @submit.prevent>
       <input @submit.prevent="checkGuess" v-model="userGuess" type="text" placeholder="Enter guess">
       <button @click="checkGuess">Check Guess</button>
     </form>
     <p class="wrongGuess">You guess incorrectly</p>
-    <div class="hintbox">
-      <button @click="showHint(1)"> Show hint #1 </button>
+    <template v-for="n in 3" v-bind:key="n.id">
+      <button @click="showHint(n)" :disabled="current_time < n*60"> Show hint #{{ n }} </button>
+    </template>
+    <div id="hintContainer">
+      <img v-if="hintToShow > -1" :src="getHintImagePath()" alt="hint">
     </div>
   </div>
 </template>
 
 <script>
+const HINTS = ["julius-caesar.jpg", "cypher.webp", "age-of-empires.jpg"]
 export default {
   name: 'HelloWorld',
   props: {
@@ -24,21 +28,40 @@ export default {
   data() {
     return {
       userGuess: '',
+      hintToShow: -1,
+      loaded_time: null,
+      interval: null,
+      current_time: null
     }
   },
   methods: {
     checkGuess() {
-      if (this.userGuess.toLowerCase() === 'hello') {
+      if (this.userGuess.toLowerCase() === 'above the fridge') {
         alert('You guessed correctly!')
       } else {
         alert('You guessed incorrectly!')
       }
     },
     showHint(hintNumber) {
-      if (hintNumber === 1) {
-        alert('The answer is hello')
+      this.hintToShow = hintNumber
+    },
+    getHintImagePath() {
+      if (this.hintToShow > -1) {
+        return require('../assets/' + HINTS[this.hintToShow - 1])
       }
     }
+  },
+  beforeUnmount(){
+    // prevent memory leak
+    clearInterval(this.interval)
+  },
+  created() {
+    this.loaded_time = new Date()
+    // update the time every second
+    this.interval = setInterval(() => {
+      const cur_time = new Date()
+      this.current_time = Math.floor((cur_time - this.loaded_time)/1000)
+    }, 1000)
   }
 }
 </script>
